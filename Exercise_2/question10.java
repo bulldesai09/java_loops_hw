e4a88eeb6d8a56b2843e6828434efac5c1b48c08
bulldesai09 <bulldesai@gmail.com>
2018-03-26 15:02:44 +0530

    Question 10 Done

---
+++ Exercise_2/question10.java
@@ -0,0 +1,16 @@
+public class question10{
+
+	public static void main(String[] args){
+		int n=10;
+		for(int i=0;i<n;i++){
+			for(int j=0;j<i;j++){
+				if(i%2!=0){
+				System.out.print("*");	
+				}
+				
+
+			}
+			System.out.print("\n");
+		}
+	}
+}
