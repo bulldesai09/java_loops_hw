65b89abc76988283b0c3120a39b4bd99f1acda1f
bulldesai09 <bulldesai@gmail.com>
2018-03-26 14:33:26 +0530

    Question 8 Done

---
+++ Exercise_2/question8.java
@@ -0,0 +1,12 @@
+public class question8{
+	public static void main(String[] args){
+		int n=5;
+		for(int i=n;i>0;i--){
+			for(int j=i;j>0;j--){
+				System.out.print("*");
+
+			}
+			System.out.print("\n");
+		}
+	}
+}
